136bbd6ea9ca4c28bb2cd2f606c27d10b321c8aa
Ilyas Turki <turki.ilyass@gmail.com>
2026-05-02 01:04:14 +0200

    refactor(extension): expose permissions via passthru, name firefox guid

--- extension.nix
+++ extension.nix
@@ -3,6 +3,10 @@
   stdenv,
   fetchurl,
 }:
+let
+  # Firefox application GUID — required by the global extensions install path.
+  firefoxAppId = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "claudezilla-firefox-extension";
   version = "0.6.4";
@@ -12,19 +16,25 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-nDYNE2iWrjHWSrUmifm8GJeI0YC1nJ6jkD1TncH6nuM=";
   };
 
-  preferLocalBuild = true;
-  allowSubstitutes = true;
-
   dontUnpack = true;
 
   passthru = {
     addonId = "claudezilla@boot.industries";
+    mozPermissions = [
+      "nativeMessaging"
+      "tabs"
+      "activeTab"
+      "<all_urls>"
+      "webRequest"
+      "webRequestBlocking"
+      "storage"
+    ];
   };
 
   installPhase = ''
     runHook preInstall
 
-    dst=$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}
+    dst=$out/share/mozilla/extensions/${firefoxAppId}
     mkdir -p "$dst"
     install -m644 "$src" "$dst/${finalAttrs.passthru.addonId}.xpi"
 
@@ -36,14 +46,11 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://addons.mozilla.org/en-US/firefox/addon/claudezilla/";
     license = lib.licenses.mit;
     platforms = lib.platforms.all;
-    mozPermissions = [
-      "nativeMessaging"
-      "tabs"
-      "activeTab"
-      "<all_urls>"
-      "webRequest"
-      "webRequestBlocking"
-      "storage"
+    maintainers = [
+      {
+        name = "Ilyas Turki";
+        github = "ilyasturki";
+      }
     ];
   };
 })
